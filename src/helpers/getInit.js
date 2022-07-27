d34730350cf92a972166ea5f0eee8ec1bb4e0b39
GevKerobyan <kerobyangevorg@gmail.com>
2022-07-27 16:11:00 +0400

    fix: initial location fixed

--- src/helpers/getInit.js
+++ src/helpers/getInit.js
@@ -3,9 +3,16 @@ import getWeather from './getWeather';
 
 function getInit({ state, dispatch }) {
 	let place;
-	axios.get('https://ipapi.co/json').then((res) => {
-		place = res.data.city;
-		getWeather({ state, dispatch, place });
+
+	axios.get('https://ipapi.co/json').then((result) => {
+		axios
+			.get(
+				`${process.env.REACT_APP_API_BASE_URL}/ip.json?key=c1d1c10cbda24036a2372844221407&q=${result.data.ip}`
+			)
+			.then((res) => {
+				place = res.data.city;
+				getWeather({ state, dispatch, place });
+			});
 	});
 }
 
